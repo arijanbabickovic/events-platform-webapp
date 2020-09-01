57657cb3277ea57ec614e133dea6396d45353f46
arijantes <arijanfs@gmail.com>
2020-09-01 09:26:25 +0000

    commented out problematic script

--- deploy-scripts/db-config.sh
+++ deploy-scripts/db-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 # Go to the deployment directory
-# cd /var/www/events/deployment
+cd /var/www/events/deployment
 
 # Remove if there is an existing database configuration
-# rm -f config/database.yml
+rm -f config/database.yml
 
 # Link the database configuration to shared db config file
-# ln -s /var/www/events/shared/config/database.yml config/database.yml
+ln -s /var/www/events/shared/config/database.yml config/database.yml
