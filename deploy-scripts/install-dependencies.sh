41a58c27730d57be54fd6ebf0f5b175f8f50b795
arijantes <arijanfs@gmail.com>
2020-09-07 16:18:03 +0000

    test

---
+++ deploy-scripts/install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /var/www/events/deployment
+yum update -y
+
+
+# Installing ruby version 2.4
+yum install ruby24 -y
+# Installing ruby development kit (needed to build gems)
+yum install ruby24-devel.x86_64 -y
+# Installing c++ garbage collector (needed to build gems)
+yum install gcc-c++ -y
+
+# Setting ruby version 2.4 as default
+alternatives --set ruby /usr/bin/ruby2.4
+gem2.4 install bundler --bindir /usr/bin
