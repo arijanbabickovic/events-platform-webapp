b2efcf4f01a725bf66ea78dac0d5db5be9fee28f
arijantes <arijanfs@gmail.com>
2020-09-07 17:34:53 +0000

    test

--- deploy-scripts/install-gems.sh
+++ deploy-scripts/install-gems.sh
@@ -9,9 +9,9 @@ git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
 # Install gems using bundler
-# - Bundle location: /var/www/pisirpaylas/shared/bundle
-# - Binary location: /var/www/pisirpaylas/shared/bin
+# - Bundle location: /var/www/pisirpaylas/bundle
+# - Binary location: /var/www/pisirpaylas/bin
 # - Without development and test gems
 # - Using gemfile in current directory
 # - Also quietly. No need to generate all logs.
-RAILS_ENV=production bundle install --binstubs /var/www/events/shared/bin --gemfile ./Gemfile --path /var/www/events/shared/bundle --without development test --deployment --quiet
+RAILS_ENV=production bundle install --binstubs /var/www/events/bin --gemfile ./Gemfile --path /var/www/events/bundle --without development test --deployment --quiet
